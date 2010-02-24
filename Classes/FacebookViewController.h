88a43f491293f235796609ccb97208eb0608041d
Frank McAuley <frank.mcauley@gmail.com>
2010-02-23 19:15:53 -0500

    Added the facebook header

---
+++ Classes/FacebookViewController.h
@@ -0,0 +1,17 @@
+//
+//  FacebookViewController.h
+//  Tattoo IQ Quiz
+//
+//  Created by Frank McAuley on 2/23/10.
+//  Copyright 2010 Frank McAuley LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FBConnect/FBConnect.h"
+
+
+@interface FacebookViewController : UIViewController {
+
+}
+
+@end
