2137afcb4475876aa95c2026dfa6a60fcb34a47e
Frank McAuley <frank.mcauley@gmail.com>
2010-03-25 23:28:58 -0400

    Updates twitter to have activityInticator and show reccenttweets, need to make sure that the users tweets show up

--- Classes/TwitterViewController.h
+++ Classes/TwitterViewController.h
@@ -10,6 +10,14 @@
 	NSString *theMessage;
 	UITextField *username;
 	UITextField *password;
+	UIActivityIndicatorView *activityIndicator;
+	UITextView *tweetsView;
+	NSMutableData *tweetsData;
+	NSMutableString *tweetsString;
+	NSMutableDictionary *currentTweetDict;
+	NSString *currentElementName;
+	NSMutableString *currentText;
+	NSSet *interestingTags;
 
 }
 
@@ -18,6 +26,8 @@
 @property (assign)NSString *theMessage;
 @property (nonatomic, retain) IBOutlet UITextField *username;
 @property (nonatomic, retain) IBOutlet UITextField *password;
+@property (nonatomic, retain) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (nonatomic, retain) IBOutlet UITextView *tweetsView;
 
 -(IBAction)goBackOneScreen:(id)sender;
 -(IBAction)setUpdateToTwitter;
--- Classes/TwitterViewController.m
+++ Classes/TwitterViewController.m
@@ -1,12 +1,80 @@
 
 #import "TwitterViewController.h"
 
+#define INTERESTING_TAG_NAMES @"text",@"name",nil
 
 
 @implementation TwitterViewController
 
 
 @synthesize textView, theMessage, username, password;
+@synthesize activityIndicator,tweetsView;
+
+//////////////NEW CODE
+-(void)startParsingTweets {
+	NSXMLParser *tweetParser = [[NSXMLParser alloc]initWithData:tweetsData];
+	tweetParser.delegate = self;
+	[tweetParser parse];
+	[tweetParser release];
+}
+
+- (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI
+ qualifiedName:(NSString *)qualifiedName
+	attributes:(NSDictionary *)attributeDict { if ([elementName isEqualToString:@"status"]) {
+	[currentTweetDict release]; currentTweetDict = [[NSMutableDictionary alloc]
+													initWithCapacity: [interestingTags count]];
+}
+	else if ([interestingTags containsObject:elementName]){
+		currentElementName = elementName;
+		currentText = [[NSMutableString alloc]init];
+	}
+}
+
+-(void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string {
+	[currentText appendString:string];
+}
+
+-(void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName 
+ namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName
+{
+	if ([elementName isEqual:currentElementName]) {
+		[currentTweetDict setValue:currentText forKey:currentElementName];
+	}
+	else if	([elementName isEqualToString:@"status"]){
+		[tweetsString appendFormat:@"%@: %@\n\n", 
+		 [currentTweetDict valueForKey:@"name"], 
+		 [currentTweetDict valueForKey:@"text"]];
+	} [currentText release]; 
+	currentText = nil;
+}
+
+
+-(void)parserDidEndDocument:(NSXMLParser *)parser {
+	tweetsView.text = tweetsString;
+}
+
+
+-(void)parserDidStartDocument:(NSXMLParser *)parser{
+	[tweetsString release];
+	tweetsString = [[NSMutableString alloc]
+					initWithCapacity:(20 *(140 + 20))];
+	currentElementName = nil;
+	currentText = nil;
+}
+
+
+-(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data{
+	[tweetsData appendData:data];
+}
+
+-(void) connectionDidFinishLoading:(NSURLConnection *)connection {
+	
+	[activityIndicator stopAnimating];
+	[self startParsingTweets];
+}
+
+
+//////////////////////
 
 
 -(IBAction)goBackOneScreen:(id)sender{
@@ -55,6 +123,22 @@
 	NSData* result = [NSURLConnection sendSynchronousRequest:therequest returningResponse:&response error:&error];
 	NSLog(@"%@", [[[NSString alloc]initWithData:result encoding:NSASCIIStringEncoding] autorelease]);
 	
+	
+	 //new code
+	
+	tweetsView.text = @"";
+	[tweetsData release];
+	tweetsData = [[NSMutableData alloc]init];
+	NSURL *url = [NSURL URLWithString:	
+				  @"http://twitter.com/statuses/public_timeline.xml"];
+	
+	NSURLRequest *request = [[NSURLRequest alloc]initWithURL:url];
+	NSURLConnection *connection = [[NSURLConnection alloc]
+								   initWithRequest:request delegate:self];
+	[connection release];
+	[request release];
+	[activityIndicator startAnimating];
+	
 	//add logic that will tell if the update has occured! 3-25-10
 }
 
@@ -65,6 +149,7 @@
     [super viewDidLoad];
 	NSString *update = [[NSString alloc] initWithString:@"Update your Twitter status with results from this Tattoo IQ test."];
 	textView.text = update;
+	interestingTags = [[NSSet alloc]initWithObjects:INTERESTING_TAG_NAMES];
 	[update release];
 	
 }
